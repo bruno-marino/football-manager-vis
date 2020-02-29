3c28e93f0051d413e14348ff8615cb70b1edbaad
Bruno Marino <48441496+bruno-marino@users.noreply.github.com>
2020-02-29 19:05:04 +0100

    adjusting map

--- src/index.js
+++ src/index.js
@@ -17,9 +17,9 @@ var svg = d3.select("svg"),
 // Map and projection
 var path = d3.geoPath();
 var projection = d3.geoMercator()
-  .scale(70)
-  .center([0,20])
-  .translate([width / 2, height / 2]);
+  .scale(100)
+  .center([0,40])
+  .translate([width /2, height /2]);
 
 // Data and color scale
 var data = d3.map();
