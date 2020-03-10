0a1bb5f783e5a9b35f56c27dc400008cf0a8400f
Bruno Marino <48441496+bruno-marino@users.noreply.github.com>
2020-03-11 00:02:49 +0100

    Radar chart beta

--- src/views/index.js
+++ src/views/index.js
@@ -1,5 +1,6 @@
 import mapchart from './map'
+import radarchart from './radar'
 import './style.scss'
 
 // export default { map, scatterplot, ... }
-export default { mapchart }
+export default { mapchart, radarchart }
