f1f45fdccc901eae337d71c08445b36f1e271e1b
Bruno Marino <48441496+bruno-marino@users.noreply.github.com>
2020-05-16 12:25:47 +0200

    z-score as country strenght per role

--- src/controller/util/country-strength-per-role.js
+++ src/controller/util/country-strength-per-role.js
@@ -10,8 +10,17 @@ export default function countryStrengthPerRole() {
      data[player.country_code].push(player.computeAvgRoleSkill(this.actualRole));
     }
   });
+
+  let num_countries=0;
+  let sum = 0;
+  let avg;
+  let r=0;
+  let variance;
+  let standard_dev;
+
   
   Object.keys(data).forEach(country => {
+
     data[country].sort((a,b) => b - a);
 
     let max = data[country].length >= 500 ? 500 : data[country].length;
@@ -20,9 +29,34 @@ export default function countryStrengthPerRole() {
       result += data[country][i];
     }
 
-    data[country] = result / max;
+    //data[country] = result / max;//first try
+    //data[country] = (result/max) * [(500 - (500 - max)) / 500];
+    //data[country] = (result/max) * (max/500);
+    //data[country] = result/500;
+    data[country] = result/500;
+    /* alternative with less malus*/
+    //let adjNP = data[country].length <= 400 ? max+50 : max;
+    //data[country] = (result/max) * (adjNP/500); 
+
+    num_countries++;
+    sum = sum + data[country];
   });
 
-  // apply malus
+  avg = sum/num_countries;
+
+  //variance computation
+  Object.keys(data).forEach(country => {
+    r = Math.pow((data[country]-avg),2)+r;
+  });
+  variance = r/num_countries;
+
+  //standard dev computation
+  standard_dev = Math.sqrt(variance);
+
+  //z-score computation for each country
+  Object.keys(data).forEach(country => {
+    data[country] = (data[country] - avg) / standard_dev;
+  });
+
   return data;
 }
